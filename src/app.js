c6eb17d8817f299c298d4b526dca7217b3099d26
BajiShaik85 <bajishaik170085@gmail.com>
2024-09-29 19:26:59 +0530

    Crud Operations

--- src/app.js
+++ src/app.js
@@ -1,21 +1,82 @@
 const express = require("express");
 const connectDb = require("./config/database");
 const app = express();
+
 const User = require("./models/user");
+
+app.use(express.json());
 app.post("/signup", async (req, res) => {
-  const user = new User({
-    firstName: "Baji",
-    lastName: "Shaik",
-    emailId: "baji@shaik.com",
-    age: 22,
-    gender: "Male",
-  });
+  const user = new User(req.body);
 
   try {
     await user.save();
     res.send("User Added");
   } catch (error) {
     res.status(400).send("Error in saving the data:" + err.message);
+  }
+});
+//Get user by Email
+app.get("/user", async (req, res) => {
+  const userEmail = req.body.emailId;
+
+  try {
+    const user = await User.findOne({ emailId: userEmail });
+    if (!user) {
+      res.status(404).send("User Not FOund");
+    } else {
+      res.send(user);
+    }
+
+    // if (users.length === 0) {
+    //   res.status(404).send("User Not FOund");
+    // } else {
+    //   res.send(users);
+    // }
+  } catch (err) {
+    res.status(400).send("Error Occured" + err.message);
+  }
+});
+
+app.get("/feed", async (req, res) => {
+  try {
+    const feedData = await User.find({});
+    if (feedData.length === 0) {
+      res.status(404).send("No Feed Found");
+    } else {
+      res.send(feedData);
+    }
+  } catch (err) {
+    res.status(400).send("Error Occured" + err.message);
+  }
+});
+app.delete("/user", async (req, res) => {
+  const userId = req.body.userId;
+  try {
+    if (!userId) {
+      res.status(404).send("No Id Found");
+    } else {
+      const user = await User.findByIdAndDelete({ _id: userId });
+      if (!user) {
+        // If no user is found to delete, return a 404 response
+        res.status(404).send("User Not Found");
+      } else {
+        // If a user was found and deleted, send success response
+        res.send("User Deleted");
+      }
+    }
+  } catch (err) {
+    res.status(400).send("Error Occured" + err.message);
+  }
+});
+app.patch("/user", async (req, res) => {
+  const userId = req.body.userId;
+  const data = req.body;
+  console.log(data);
+  try {
+    const user = await User.findByIdAndUpdate({ _id: userId }, data);
+    res.send("User Updated");
+  } catch (error) {
+    res.status(400).send("Error Occured" + err.message);
   }
 });
 
