5adf10626c472537bc88d1feede8c2f5c3a73543
BajiShaik85 <bajishaik170085@gmail.com>
2024-09-29 10:59:12 +0530

    Exploring Get Post Delete

--- src/app.js
+++ src/app.js
@@ -1,11 +1,25 @@
 const express = require("express");
 console.log("Starting New Project!");
 const app = express();
-app.use("/", (req, res) => {
-  res.send("Hello From Server");
+
+//This will only handle GET call to /user
+app.get("/user", (req, res) => {
+  res.send({ firstName: "Baji Shaik", Course: "Node JS" });
 });
+app.post("/user", (req, res) => {
+  res.send("Data Saved Succesfully to the Database");
+});
+
+app.delete("/user", (req, res) => {
+  res.send("User Deleted!");
+});
+
+//this will match all the HTTP method API calls
 app.use("/test", (req, res) => {
   res.send("Hello From Test");
 });
 
+app.use("/", (req, res) => {
+  res.send("Hello From Server");
+});
 app.listen(5000, () => console.log("Server Running On Port 5000"));
