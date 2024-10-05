95813d07613ac6616b9f04308c55d0e5935c9e3a
BajiShaik85 <bajishaik170085@gmail.com>
2024-10-06 01:00:03 +0530

    reset password

--- src/routes/profile.js
+++ src/routes/profile.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const { userAuth } = require("../middlewares/auth");
 const { validateEditProfileData } = require("../utils/validation");
+const bcrypt = require("bcrypt");
+const validator = require("validator");
+
 const profileRoute = express.Router();
 
 profileRoute.get("/profile", userAuth, async (req, res) => {
@@ -15,7 +18,7 @@ profileRoute.get("/profile", userAuth, async (req, res) => {
   }
 });
 
-profileRoute.patch("profile/edit", userAuth, async (req, res) => {
+profileRoute.patch("/profile/edit", userAuth, async (req, res) => {
   try {
     if (!validateEditProfileData) {
       throw new Error("Invalid Edit Request");
@@ -23,8 +26,28 @@ profileRoute.patch("profile/edit", userAuth, async (req, res) => {
     const loggedInUser = req.user;
     Object.keys(req.body).forEach((key) => (loggedInUser[key] = req.body[key]));
     await loggedInUser.save();
-    res.send(`${loggedInUser.firstName},Your Profile Updated Successfully`);
-  } catch (error) {}
+    res.send(`${loggedInUser.firstName}, Your Profile Updated Successfully`);
+  } catch (err) {
+    res.status(400).send("Error Occured:" + err.message);
+  }
+});
+profileRoute.patch("/profile/password", userAuth, async (req, res) => {
+  try {
+    const strongPassword = validator.isStrongPassword(req.body.password);
+
+    if (!strongPassword) {
+      throw new Error("Please enter a strong password");
+    } else {
+      const passwordHash = await bcrypt.hash(req.body.password, 10);
+      const user = req.user;
+      user.password = passwordHash;
+      await user.save();
+
+      res.send(`${user.firstName}, Your Password changed Successfully`);
+    }
+  } catch (err) {
+    res.status(400).send("Error Occured:" + err.message);
+  }
 });
 
 module.exports = profileRoute;
