45cadd23165dd0e02dad543270ddedef5a8b57cf
BajiShaik85 <bajishaik170085@gmail.com>
2024-10-06 14:51:43 +0530

    connection request

--- src/routes/request.js
+++ src/routes/request.js
@@ -1,8 +1,29 @@
 const express = require("express");
 const { userAuth } = require("../middlewares/auth");
+const ConnectionRequest = require("../models/connectionRequest");
 const requestRouter = express.Router();
-requestRouter.post("/sendConnectionRequest", userAuth, async (req, res) => {
-  const user = req.user;
-  console.log(user.firstName + " Sent you a request");
-});
+requestRouter.post(
+  "/request/send/:status/:toUserId",
+  userAuth,
+  async (req, res) => {
+    try {
+      const fromUserId = req.user._id;
+      const toUserId = req.params.toUserId;
+      const status = req.params.status;
+      const allowedStatus = ["ignored", "interested"];
+      if (!allowedStatus.includes(status)) {
+        return res.status(400).json({ message: "Invalid Status Type", status });
+      }
+      const connectionRequest = new ConnectionRequest({
+        fromUserId,
+        toUserId,
+        status,
+      });
+      const data = await connectionRequest.save();
+      res.json({ message: "Connection request sent successfully", data });
+    } catch (err) {
+      return res.status(500).send("Error Occured:" + err.message);
+    }
+  }
+);
 module.exports = requestRouter;
